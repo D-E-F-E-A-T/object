c9f427a9e0563ab3c6cfd07b311f6c0376a23111
pestonoire <pestonoire@users.noreply.github.com>
2015-12-14 01:38:59 -0600

    test

--- object.lua
+++ object.lua
@@ -4,15 +4,15 @@
 local object = {}
 
 function object:new(t)
-  return setmetatable(t or {}, { __index = self, __call = self.new })
+	return setmetatable(t or {}, { __index = self, __call = self.new })
 end
 
 function object:clone()
-  local t = {}
-  for k, v in pairs(self) do
-    t[k] = v
-  end
-  return setmetatable(t, getmetatable(self))
+	local t = {}
+	for k, v in pairs(self) do
+		t[k] = v
+	end
+	return setmetatable(t, getmetatable(self))
 end
 
 return setmetatable(object, { __call = object.new })
