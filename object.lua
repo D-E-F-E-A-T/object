f2fc18fc5f622716eb480e62b9882766f99a2652
pestonoire <pestonoire@users.noreply.github.com>
2015-12-14 01:29:25 -0600

    initial commit

---
+++ object.lua
@@ -0,0 +1,18 @@
+-- to use this module in another .lua file, use:
+-- local object = require 'object'
+
+local object = {}
+
+function object:new(t)
+  return setmetatable(t or {}, { __index = self, __call = self.new })
+end
+
+function object:clone()
+  local t = {}
+  for k, v in pairs(self) do
+    t[k] = v
+  end
+  return setmetatable(t, getmetatable(self))
+end
+
+return setmetatable(object, { __call = object.new })
